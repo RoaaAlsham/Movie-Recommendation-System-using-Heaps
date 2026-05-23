47bdd54980d2483301ffca81287cba428a204ab3
roaaalsham <roaashalabalsham@gmail.com>
2026-05-23 19:48:21 +0300

    initialize the folder structure of the project with empty files while leaving gui folder empty, it has to contain the following classes: - MainWindow.java - TargetUserPanel.java - MovieRatingPanel.java

---
+++ src/main/java/com/mycompany/movie_recommendation_system_using_heaps/models/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.movie_recommendation_system_using_heaps.models;
+
+/**
+ *
+ * @author zaher
+ */
+public class User {
+    
+}
